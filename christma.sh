f36ed714d38028a6811c2eb7200afe4ef0af8b94
ercas <ercas@users.noreply.github.com>
2015-12-25 00:21:45 -0500

    added support for vlc and aplay

--- christma.sh
+++ christma.sh
@@ -5,6 +5,10 @@ tmpdir=$(mktemp -d /tmp/christma.sh-XXXXX)
 
 audio=$tmpdir/faithnoel.ogg # nonexistant file to be downloaded later
 audioUrl="https://drive.google.com/uc?id=0B2wzVJ8L65DfV250ZGN6aHREZWs&export=download"
+audioRawUrls=( # this is an array of all .wav files to be iterated through
+    "https://drive.google.com/uc?id=0B2wzVJ8L65DfYmFQZDI5WU5aQ3M&export=download"
+    "https://drive.google.com/uc?id=0B2wzVJ8L65DfaUVMZmFBQ2tYblk&export=download"
+)
 audioPid=0
 
 frames=$tmpdir/frames # nonexistant directory to be created and filled later
@@ -41,13 +45,11 @@ function grab() {
 ########## play
 
 trap quit SIGINT SIGTERM
-
-# setup
 tput civis
+
+# setup frames
 mkdir -p $frames
 
-echo "Downloading audio..."
-grab "$audioUrl" $audio
 echo "Downloading frames..."
 grab "$framesUrl" $tmpdir/framesArchive
 
@@ -57,15 +59,37 @@ rm $tmpdir/framesArchive
 find $frames -type f -exec mv {} $frames \; 2> /dev/null
 totalFrames=$(find $frames -type f | wc -l)
 
-# play music
+# setup and play music
+function grabAudio() {
+    # this could just run once before the media player checks, but then systems
+    # with only aplay would have to download twice.
+    echo "Downloading audio..."
+    grab "$audioUrl" $audio
+}
 if checkcmd ffplay; then
+    grabAudio
     ffplay -autoexit -nodisp -loglevel 8 $audio &
 elif checkcmd play; then
+    grabAudio
     play -q $audio &
 elif checkcmd mpv; then
+    grabAudio
     mpv --really-quiet $audio &
 elif checkcmd mplayer; then
+    grabAudio
     mplayer -really-quiet -noconsolecontrols $audio 2> /dev/null &
+elif checkcmd clvc; then
+    grabAudio
+    cvlc --no-osd --quiet $audio 2> /dev/null &
+elif checkcmd aplay; then
+    # more data transfer is needed for this so it's avoided if at all possible.
+    # the file is split up into two 23M parts due to size limits of google
+    # drive's virus scanner, which makes it difficult to direct download.
+    (
+        for part in "${audioRawUrls[@]}"; do
+            grab "$part" - | aplay -q
+        done
+    ) &
 else
     echo "No suitable audio player found."
 fi
